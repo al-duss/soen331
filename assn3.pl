d0f1e7c0df960c3173c145053e3d3e9e682a5cd8
Jonathan Hamel <jo_ham@taos.encs.concordia.ca>
2015-03-26 15:04:34 -0400

    Prolog facts

---
+++ assn3.pl
@@ -0,0 +1,76 @@
+%% =========RULES================
+
+%% =========STATES=============== 
+
+%% Overall
+
+state(dormant).
+state(init).
+state(idle).
+state('error_diagnosis').
+state('safe_shutdown').
+state(monitoring).
+
+%% init
+
+state('boot_hw').
+state(senchk).
+state(tchk).
+state(psichk).
+state(ready).
+
+%% monitor
+
+state(monidle).
+state('regulate_environment').
+state(lockdown).
+
+%% lockdown
+
+state('prep_purge').
+state('alt_temp').
+state('alt_psi').
+state('risk_assess').
+state('safe_status').
+
+%% error_diagnosis
+
+state('error_rev').
+state('applicable_rescue').
+state('reset_module_data').
+
+
+%% =========initial states========
+
+initial_state(dormant).
+initial_state('boot_hw').
+initial_state('error_rev').
+initial_state(monidle).
+initial_state('prep_purge').
+
+%% =========Superstates==========
+
+%% init
+
+superstate(init, 'boot_hw').
+superstate(init, senchk).
+superstate(init, tchk).
+superstate(init, psichk).
+superstate(init, ready).
+
+%% monitor
+
+superstate(monitor, monidle).
+superstate(monitor, lockdown).
+superstate(monitor, regulate_environment).
+
+%% lockdown
+
+superstate(lockdown, 'prep purge').
+superstate(lockdown, 'alt_temp').
+superstate(lockdown, 'alt_psi').
+superstate(lockdown, 'risk_assess').
+superstate(lockdown, 'safe_status').
+
+
+
